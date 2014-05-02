e84662ab38b96ae0a91a7b1ed6893ad12251bc59
ddffx <debabrata.das@gmail.com>
2014-05-02 13:35:41 -0400

    adding grunt, bower sailsjs

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,22 @@ sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make  
 sudo add-apt-repository ppa:chris-lea/node.js  
 sudo apt-get update  
-sudo apt-get install nodejs 
+sudo apt-get install nodejs << EOF
+Y
+EOF
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10  
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list  
 apt-get -y update  
 apt-get -y install mongodb-10gen  
 
-sudo apt-get install nginx 
+sudo apt-get install nginx << EOF
+Y
+EOF
 update-rc.d nginx defaults
+
+npm install -g grunt-cli
+
+npm install -g bower
+
+npm -g install sails
